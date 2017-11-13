87443297d336ee441330125d126f6ac9b0201172
Jessie Huff <huffjd1@gmail.com>
2017-11-13 18:09:02 +0000

    Cause Controller just needs a reading mechanism, not creating or editing as it's a part of opportunity

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -2,51 +2,18 @@ class CausesController < ApplicationController
 
   # GET: /causes
   get "/causes" do
-    if logged_in? 
-      @causes = Cause.all 
+    if logged_in?
+      @causes = Cause.all
       erb :"/causes/index.html"
-    else 
+    else
       redirect '/login'
     end
   end
 
-  # GET: /causes/new
-  get "/causes/new" do
-    if logged_in? 
-      erb :"/causes/new.html"
-    else 
-      redirect '/login'
-    end
-  end
-
-  # POST: /causes
-  post "/causes" do
-    if !params.value?("") 
-      @cause = Cause.new(params)
-      current_user.causes << @cause 
-      redirect "/causes"
-    else 
-      redirect '/causes/new' 
-    end
-  end
 
   # GET: /causes/5
   get "/causes/:id" do
     erb :"/causes/show.html"
   end
 
-  # GET: /causes/5/edit
-  get "/causes/:id/edit" do
-    erb :"/causes/edit.html"
-  end
-
-  # PATCH: /causes/5
-  patch "/causes/:id" do
-    redirect "/causes/:id"
-  end
-
-  # DELETE: /causes/5/delete
-  delete "/causes/:id/delete" do
-    redirect "/causes"
-  end
 end
