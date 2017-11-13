b08eb6b030e330e450240b4c1d72421b20e8a49c
Jessie Huff <huffjd1@gmail.com>
2017-11-13 19:54:20 +0000

    Added a search results view

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -2,4 +2,8 @@ class Opportunity < ActiveRecord::Base
   belongs_to :volunteer
   has_many :opportunity_causes
   has_many :causes, through: :opportunity_causes
+
+  def self.search(search) 
+    where("cause like ?", "%#{search}%")
+  end
 end
